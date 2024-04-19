3730abfed44b0167ef39af1c07a127e09baddd3b
GABI <vitoria.bertani@gmail.com>
2024-04-19 16:19:35 -0400

    Javascript

---
+++ meu_site/script.js
@@ -0,0 +1,27 @@
+//console.log("oi estou vivo!");//
+//var age = 16;
+//let status = "oline";
+//const nome = prompt("vitoria");
+
+//const h1 = document.getElementById("texto");
+//h1.innerHTML = "ola seja bem vindo " + nome
+
+// const valor1 = prompt("insira o segundo valor");
+// const valor2 = prompt(" insira o valor");
+// const operacao = prompt("PARA somar digite SOMAR");
+// const resultado = parseInt (valor1) + parseInt(valor2);
+// const element = document.getElementById("texto");
+
+// if (resultado && operacao == "SOMAR") {
+//     element.innerHTML = " o resultado da soma é " + resultado;
+// }else{
+//     element.innerHTML = " voce nao esta somando";
+// }
+let numero = 10;
+while (numero > 0) {
+    console.log(numero);
+    numero = numero - 1;
+}
+const frutas ={"banana", " maça", " melancia"};
+frutas.map((frutas) => console.log(fruta);
+
